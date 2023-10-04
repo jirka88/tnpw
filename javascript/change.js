3f83044c584a605e9c37787ef78eec9b579a1c95
jn88 <laval@centrum.cz>
2023-10-04 23:09:23 +0200

    improves

--- javascript/change.js
+++ javascript/change.js
@@ -6,27 +6,36 @@ function change(id) {
 
     switch (id) {
         case 1:
-            title.innerText = "Introduction to network"
-            date.innerText = "2020"
-            image.src = "/img/certificates/IntroductionToNetwork.png";
-            image.alt = "Introduction to network certifikát"
+            title.innerText = "IT Essentials"
+            date.innerText = "červen 2020"
+            image.src = "/img/certificates/ItEssentials.jpg";
+            image.alt = "Cisco IT Essentials"
             break;
         case 2:
-            date.innerText = "2021"
+            title.innerText = "Introduction to network"
+            date.innerText = "červenec 2021"
+            image.src = "/img/certificates/IntroductionToNetwork.png";
+            image.alt = "Cisco Introduction to network"
             break;
         case 3:
+            title.innerText = "Switching, Routing and Wireless Essentials"
+            date.innerText = "říjen 2022"
+            image.src = "/img/certificates/CCNAv7.jpg"
+            image.alt = "Cisco Switching, Routing and Wireless Essentials"
+            break;
+        case 4:
             title.innerText = "Tutorial css"
             date.innerText = "září 2023"
             image.src = "/img/certificates/Coursera%202UHDG42AG36X_page-0001.jpg"
             image.alt = "tutorial css certifikát"
             break;
-        case 4:
+        case 5:
             title.innerText = "Introduction to .NET Core"
             date.innerText = "říjen 2023"
             image.src = "/img/certificates/Coursera%20JKLR57RFHV6C_page-0001.jpg";
             image.alt = "Introduction to .NET Core certifikát"
             break;
-        case 5:
+        case 6:
             title.innerText = "React Basics"
             date.innerText = "září 2023"
             image.src = "/img/certificates/Coursera%20JVGEQWTZ36FX_page-0001.jpg";
